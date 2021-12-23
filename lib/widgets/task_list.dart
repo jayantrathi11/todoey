3e8689f21f92ea517f56956fe44a557aaca1c8ef
jayantrathi <jayantrathi955@gmail.com>
2021-12-23 17:39:39 +0530

    New Commit

--- lib/widgets/task_list.dart
+++ lib/widgets/task_list.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:todoey_list/widgets/task_tile.dart';
 
-class TasksList extends StatelessWidget {
+class TasksList extends StatefulWidget {
   const TasksList({
     Key? key,
   }) : super(key: key);
 
+  @override
+  State<TasksList> createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
   @override
   Widget build(BuildContext context) {
     return ListView(
