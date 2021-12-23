9101ce50e83ddebb34e2bfd5c62c1aad538e9c4f
jayantrathi <jayantrathi955@gmail.com>
2021-12-23 15:46:23 +0530

    changes

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,25 +1,49 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
-  const TaskTile({
-    Key? key,
-  }) : super(key: key);
+class TaskTile extends StatefulWidget {
+  @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+  late Function func;
+  void callback(bool? value) {
+    setState(() {
+      isChecked = !isChecked;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: const Text(
+      title: Text(
         'Padosi ka sir phodna',
         style: TextStyle(
           fontSize: 20,
           fontWeight: FontWeight.w600,
+          decoration: isChecked ? TextDecoration.lineThrough : null,
         ),
       ),
-      trailing: Checkbox(
-        value: true,
-        checkColor: Colors.white,
-        onChanged: (bool? okay) {},
+      trailing: TaskCheckbox(
+        checkboxstate: isChecked,
+        func: callback,
       ),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  const TaskCheckbox({required this.checkboxstate, required this.func});
+  final bool checkboxstate;
+  final Function(bool?) func;
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: checkboxstate,
+      activeColor: Colors.lightBlueAccent,
+      checkColor: Colors.white,
+      onChanged: func,
     );
   }
 }
